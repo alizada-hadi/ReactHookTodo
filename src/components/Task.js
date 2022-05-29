f963cd16f24ee97e485d1eb1d41ef0c2eefa10a2
Hadi <alizadahadi2050@gmail.com>
2022-05-29 17:46:19 +0430

    Change the previous way of listing the task items, and adjust them in a row

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,10 +4,16 @@ function Task({task, completedTask, index, removeTask}) {
   return (
     <div className=''>
       <div className='my-2' style={{textDecoration : task.completed ? "line-through" : ""}}>
-          {task.title}
+          <div className="row">
+            <div className="col-lg-6">
+              {task.title}
+            </div>
+            <div className="col-lg-6">
+              <button className='btn float-right btn-primary mx-2 mb-3' onClick={() => completedTask(index)}>Complete</button>
+              <button className='btn btn-danger float-right mx-2 mb-3' onClick={() => removeTask(index)}>Remove</button>
+            </div>
+          </div>
       </div>
-          <button className='btn btn-primary mx-2 mb-3' onClick={() => completedTask(index)}>Complete</button>
-          <button className='btn btn-danger mx-2 mb-3' onClick={() => removeTask(index)}>Remove</button>
     </div>
   )
 }
